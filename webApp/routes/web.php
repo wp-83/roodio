c78e1199de867257aa82515589f31c792cc63c66
William P. <williampratama837@gmail.com>
2025-12-25 11:05:52 +0700

    add: login page and supporting page

--- webApp/routes/web.php
+++ webApp/routes/web.php
@@ -9,3 +9,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('503error');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/sign-up', function () {
+    return view('register');
+});
+
+Route::get('/forget-password', function () {
+    return view('forgetPass');
+});
