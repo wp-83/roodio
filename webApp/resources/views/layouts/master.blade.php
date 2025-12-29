5298125134aeadd5bdb7c5a93c0fede333e34bc5
William P. <williampratama837@gmail.com>
2025-12-29 21:08:06 +0700

    add: master layout

---
+++ webApp/resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'ROODIO Page')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('style')
+    @stack('script')
+</head>
+<body class="container-body @yield('bodyClass', '')">
+    @yield('bodyContent')
+</body>
+</html>
