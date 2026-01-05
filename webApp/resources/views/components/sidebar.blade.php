be9db3a6ec16d9c425c1ffaf4b7e5cfabdabb1b6
William P. <williampratama837@gmail.com>
2026-01-05 21:44:29 +0700

    update: sidebar style

--- webApp/resources/views/components/sidebar.blade.php
+++ webApp/resources/views/components/sidebar.blade.php
@@ -1,19 +1,17 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-
 @props([
     'mood' => 'relaxed',
-    'isToggle' => true
+    'isToggle' => false
 ])
 
 {{-- belom passing route untuk masing2 button --}}
 <div
 {{ 
     $attributes->merge([
-        'class' => 'flex flex-col gap-5 w-fit bg-primary-85 h-full pt-10 ' . (($isToggle) ? 'px-4 ' : ' ')
+        'class' => 'flex flex-col gap-5 w-fit bg-primary-85 h-full ' . (($isToggle) ? ' px-4 ' : ' ')
     ])
 }}
 >
-    <x-sidebarButton mood="{{ $mood }}" icon='home' label='Home' content="Let's play the music!" route='awikwok' isToggle='{{ $isToggle }}'></x-sidebarButton>
+    <x-sidebarButton mood="{{ $mood }}" icon='home' label='Home' content="Let's play the music!" isToggle='{{ $isToggle }}'></x-sidebarButton>
     <x-sidebarButton mood="{{ $mood }}" icon='forum' label='Forum' content="Be part of the discussion!" isToggle='{{ $isToggle }}'></x-sidebarButton>
     <x-sidebarButton mood="{{ $mood }}" icon='social' label='Social' content="Connect with others now!" isToggle='{{ $isToggle }}'></x-sidebarButton>
     <x-sidebarButton mood="{{ $mood }}" icon='mood' label='Mood' content="Let's see your mood history!" isToggle='{{ $isToggle }}'></x-sidebarButton>
