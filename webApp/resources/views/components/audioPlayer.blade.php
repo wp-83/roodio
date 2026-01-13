1dff55ee1d7f56f2f7829867833216cdfa9c76fe
William P. <williampratama837@gmail.com>
2026-01-13 16:29:16 +0700

    add: audio player button component

--- webApp/resources/views/components/audioPlayer.blade.php
+++ webApp/resources/views/components/audioPlayer.blade.php
@@ -1,3 +1,21 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+@php
+    
+@endphp
+
+
+<div class='w-full h-20 bg-primary-85 relative'>
+    <div class='w-full h-1 bg-white'></div>
+    {{-- <div class='h-10 w-10 rounded-full bg-white'></div> --}}
+    <div>
+        <div>
+            
+        </div>
+    </div>
+</div>
+
 <div>
     <!-- Well begun is half done. - Aristotle -->
 </div>
--- webApp/resources/views/layouts/main.blade.php
+++ webApp/resources/views/layouts/main.blade.php
@@ -20,9 +20,10 @@
         <div class='flex flex-row flex-1 min-h-0 relative'>
             <x-sidebar></x-sidebar>
             <div class='w-full bg-primary-100 overflow-x-hidden overflow-y-auto min-h-0 p-10'>
-                <p class='text-9xl text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident distinctio ex quasi sint debitis enim nesciunt id impedit eveniet ipsum molestias aspernatur blanditiis mollitia, voluptas saepe sequi, inventore assumenda corrupti?</p>
+                <p class='text-8xl text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident distinctio ex quasi sint debitis enim nesciunt id impedit eveniet ipsum molestias aspernatur blanditiis mollitia, voluptas saepe sequi, inventore assumenda corrupti?</p>
             </div>
         </div>
     </div>
-    <div class='w-full h-16 bg-primary-85 shrink-0'></div>
+    <x-audioPlayer></x-audioPlayer>
+    {{-- <div class='w-full h-16 bg-primary-85 shrink-0'></div> --}}
 @endsection
