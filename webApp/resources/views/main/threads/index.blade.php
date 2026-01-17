455667f5b24295cae19b73f786f071a04505c58a
William P. <williampratama837@gmail.com>
2026-01-17 14:12:38 +0700

    add: aos init

--- webApp/resources/views/main/threads/index.blade.php
+++ webApp/resources/views/main/threads/index.blade.php
@@ -1,13 +1,12 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Threads</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@extends('layouts.main')
+
+
+@push('head')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-</head>
-<body>
+@endpush
+
+
+@section('mainContent')
     <a href="{{ route('thread.create') }}">Add thread</a>
     @forelse($threads as $thread)
     <div class="">
@@ -42,12 +41,4 @@
     @endforelse
 
     <x-threadBox></x-threadBox>
-</body>
-</html> --}}
-
-@php
-    // $mood = 'relaxed';
-    // $fullname = 'William';
-@endphp
-
-@extends('layouts.main')
+@endsection
