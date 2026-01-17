e711a7478c07041ed7f1ffab0b4ffb8e198d418b
William P. <williampratama837@gmail.com>
2026-01-17 14:00:46 +0700

    update: head element stack

--- webApp/resources/views/layouts/master.blade.php
+++ webApp/resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'ROODIO')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('head')
     @livewireStyles
     @stack('style')
     @livewireScripts
