72a39c975ecce41b0f0da6d0f9bef95d17948dc2
William P. <williampratama837@gmail.com>
2026-01-17 13:38:58 +0700

    update: main layouts restructuring

--- webApp/resources/views/layouts/main.blade.php
+++ webApp/resources/views/layouts/main.blade.php
@@ -15,7 +15,7 @@
 @section('bodyContent')
     @yield('overlayContent')
     <div class="shrink-0">
-        <x-navbar :mood='$mood'></x-navbar>
+        <x-navbar :mood='$mood' :fullname='$fullname' :username='$username' :profilePhoto='$profilePhoto'></x-navbar>
     </div>
     <div class='flex flex-col flex-1 min-h-0'>
         <div class='flex flex-row flex-1 min-h-0 relative'>
