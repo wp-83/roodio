a0fee8368efd10fa8c8e08c8f7a21f3de722e719
William P. <williampratama837@gmail.com>
2026-01-18 00:48:34 +0700

    update: thread page

--- webApp/app/Models/Thread.php
+++ webApp/app/Models/Thread.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Thread extends Model
@@ -38,5 +39,10 @@ class Thread extends Model
     public function reactions(): HasMany
     {
         return $this->hasMany(Reactions::class, 'threadId', 'id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userId', 'id');
     }
 }
--- webApp/resources/views/components/threadBox.blade.php
+++ webApp/resources/views/components/threadBox.blade.php
@@ -1,28 +1,54 @@
 @props([
+    'mood',
     'creator',
     'createdAt',
     'title',
     'content',
-    'threadId'
+    'threadId',
+    'profilePicture'
 ])
 
-<div class='bg-secondary-angry-10 rounded-lg w-sm h-max p-5 md:w-md lg:w-2xl'>
-    <div class='flex flex-row items-center gap-5 w-full'>
-        <div class='w-16 h-16 bg-primary-10 rounded-full flex items-center justify-center relative z-5 overflow-hidden'>
-            {{-- @if (isset($user->userDetail->profilePhoto))
-                <img src="{{ config('filesystems.disks.azure.url') . '/' . $user->userDetail->profilePhoto }}" alt="{{ $user->userDetail->fullname }}" class='w-full h-full object-cover'> 
+@php
+    $bgContainer = [
+        'happy' => 'bg-secondary-happy-10',
+        'sad' => 'bg-secondary-sad-10',
+        'relaxed' => 'bg-secondary-relaxed-10',
+        'angry' => 'bg-secondary-angry-10',
+    ];
+
+    $borderMood = [
+        'happy' => 'border-secondary-happy-100',
+        'sad' => 'border-secondary-sad-100',
+        'relaxed' => 'border-secondary-relaxed-100',
+        'angry' => 'border-secondary-angry-100'
+    ];
+
+    $textMood = [
+        'happy' => 'text-secondary-happy-100',
+        'sad' => 'text-secondary-sad-100',
+        'relaxed' => 'text-secondary-relaxed-100',
+        'angry' => 'text-secondary-angry-100',
+    ];
+@endphp
+
+<div class='{{ $bgContainer[$mood] }} rounded-lg h-max p-5 w-full'>
+    <div class='flex flex-row items-center gap-2 w-full'>
+        <div class='w-18 h-18 border-2 {{ $borderMood[$mood] }} rounded-full flex items-center justify-center'>
+            <div class='w-16 h-16 bg-primary-10 rounded-full flex items-center justify-center relative z-5 overflow-hidden'>
+            @if (!empty($profilePicture))
+                <img src="{{ config('filesystems.disks.azure.url') . '/' . $profilePicture }}" alt="{{ $creator }}" class='w-full h-full object-cover'> 
             @else
-                <p class='text-paragraph font-primary font-bold text-primary-70 h-fit'>{{ Str::charAt(Str::upper($user->userDetail->fullname), 0) }}</p>
-                @endif --}}
-            <p class='text-subtitle font-primary font-bold text-primary-70 h-fit'>{{ Str::charAt(Str::upper('William'), 0) }}</p>
+                <p class='text-subtitle font-primary font-bold text-primary-70 h-fit'>{{ Str::charAt(Str::upper($creator), 0) }}</p>
+            @endif
         </div>
-        <div class='flex-1 w-full'>
-            <div class='flex flex-row justify-between'>
-                <p>{{ $creator }}</p>
-                <p>Follow</p>
+        </div>
+        <div class='flex-1 w-full font-secondaryAndButton'>
+            <div class='flex flex-col'>
+                <p class='text-body-size font-bold {{ $textMood[$mood] }}'>{{ $creator }}</p>
+                <p class='text-micro lg:text-small'>{{ $createdAt }}</p>
             </div>
             <div>
-                <p>{{ $createdAt }}</p>
+                {{-- <x-button mood='grayscale'></x-button> --}}
             </div>
         </div>
     </div>
