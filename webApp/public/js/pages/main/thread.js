c1edbc2a0e6b46a59e21450acac6c7111fe7cd81
William P. <williampratama837@gmail.com>
2026-01-20 10:38:58 +0700

    fix: scroll the newest for all reply bar

--- webApp/public/js/pages/main/thread.js
+++ webApp/public/js/pages/main/thread.js
@@ -1,3 +1,8 @@
-const replyContainer = document.getElementById('replyContainer');
+const replyContainer = document.querySelectorAll('#replyContainer');
 
-replyContainer.scrollTop = replyContainer.scrollHeight;
+// replyContainer.scrollTop = replyContainer.scrollHeight;
+
+replyContainer.forEach((reply) => {
+    reply.scrollTop = reply.scrollHeight;
+    
+});
