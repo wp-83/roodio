44f886232edfce7ced308fec0895d3e8fadcd20d
William P. <williampratama837@gmail.com>
2026-01-22 16:54:54 +0700

    update: playlist layout

--- webApp/vite.config.js
+++ webApp/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     server: {
         // host: '0.0.0.0', // Mengizinkan akses dari semua IP
         // hmr: {
-        //     host: '174.139.116.95' // GANTI dengan IP Address komputer kamu dari langkah 1, php artisan --host=0.0.0.0 --port=8000
+        //     host: '10.68.99.122' // GANTI dengan IP Address komputer kamu dari langkah 1, php artisan serve --host=0.0.0.0 --port=8000
         // },
         watch: {
             ignored: ['**/storage/framework/views/**'],
