ed8294b62fa36fa79c8d5ff0d4e921cfa696b216
William P. <williampratama837@gmail.com>
2026-02-04 15:24:14 +0700

    update: finalize thread box view

--- webApp/resources/views/components/threadBox.blade.php
+++ webApp/resources/views/components/threadBox.blade.php
@@ -65,7 +65,8 @@
 @endphp
 
 
-<div class='{{ $bgContainer[$mood] }} rounded-lg h-max p-5 w-full overflow-x-hidden lg:max-w-md'>
+<div class='{{ $bgContainer[$mood] }} relative rounded-lg h-max p-5 w-full overflow-hidden lg:max-w-md'>
+    <img src="{{ asset('assets/moods/icons/' . $mood . '.png') }}" alt="$mood" class='w-48 h-48 opacity-15 absolute right-0 top-0 translate-x-14 -rotate-145 -translate-y-13 md:w-68 md:h-68 md:translate-x-18 md:-translate-y-24'>
     <div class='flex flex-row items-center gap-2 w-full'>
         <div class='w-16 h-16 lg:w-18 lg:h-18 border-2 {{ $borderMood[$mood] }} rounded-full flex items-center justify-center'>
             <div class='w-14 h-14 lg:w-16 lg:h-16 bg-primary-10 rounded-full flex items-center justify-center relative z-5 overflow-hidden'>
@@ -107,7 +108,6 @@
                             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
 
                             <g id="SVGRepo_iconCarrier"> <title>comment 3</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-207.000000, -257.000000)" fill="{{ $elementColor[$mood] }}"> <path d="M231,273 C229.896,273 229,272.104 229,271 C229,269.896 229.896,269 231,269 C232.104,269 233,269.896 233,271 C233,272.104 232.104,273 231,273 L231,273 Z M223,273 C221.896,273 221,272.104 221,271 C221,269.896 221.896,269 223,269 C224.104,269 225,269.896 225,271 C225,272.104 224.104,273 223,273 L223,273 Z M215,273 C213.896,273 213,272.104 213,271 C213,269.896 213.896,269 215,269 C216.104,269 217,269.896 217,271 C217,272.104 216.104,273 215,273 L215,273 Z M223,257 C214.164,257 207,263.269 207,271 C207,275.419 209.345,279.354 213,281.919 L213,289 L220.009,284.747 C220.979,284.907 221.977,285 223,285 C231.836,285 239,278.732 239,271 C239,263.269 231.836,257 223,257 L223,257 Z" id="comment-3" sketch:type="MSShapeGroup"> </path> </g> </g> </g>
-
                             </svg>
                     </div>
                     <p class='font-secondaryAndButton text-primary-60 text-micro md:text-small'>10.3K</p>
