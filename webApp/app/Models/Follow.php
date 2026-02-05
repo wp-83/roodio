8253a6a305b0dd141b9687eb750ab309f63f3340
William P. <williampratama837@gmail.com>
2026-02-05 21:49:56 +0700

    update: user relatinship with userDetail

--- webApp/app/Models/Follow.php
+++ webApp/app/Models/Follow.php
@@ -2,11 +2,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\DB;
 
 class Follow extends Model
 {
     protected $fillable = ['userId', 'followedId'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userId', 'id');
+    }
 
     protected static function booted()
     {
