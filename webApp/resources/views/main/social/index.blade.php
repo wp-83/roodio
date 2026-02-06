937c6ec62854b3ebdd8e440ab876a5b180b2a455
William P. <williampratama837@gmail.com>
2026-02-06 12:55:27 +0700

    update: responsive design card profile

--- webApp/resources/views/main/social/index.blade.php
+++ webApp/resources/views/main/social/index.blade.php
@@ -79,8 +79,8 @@
         </div>
     </form>
 
-    @if (!empty($users))
-        <div class="w-full mt-4 grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7 contentFadeLoad">        
+    @if (count($users) > 0)
+        <div class="w-full mt-4 grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7 md:gap-9 contentFadeLoad">        
             @foreach($users as $user)
                 @php
                     $username = $user->username;
@@ -92,44 +92,45 @@
                     $checkSelfProfile = !($user->id == $mainUser->id);
                 @endphp
                 @if ($checkSelfProfile)
-                    {{-- <div class='w-max h-max bg-secondary-happy-10 p-4 mb-5 flex flex-col items-center rounded-md overflow-hidden'>
-                        <div>
-                            <div class='w-36 h-36 overflow-hidden {{ $bgPhotoStyle[$mood] }}'>
+                    <div class='w-full h-max text-white py-3 px-4 flex flex-row items-center rounded-md overflow-hidden font-secondaryAndButton hover:bg-white/15 md:hidden'>
+                        <div class='mr-2.5'>
+                            <div class='w-30 h-30 overflow-hidden {{ $bgPhotoStyle[$mood] }}' style="clip-path: polygon(50% 0%, 83% 12%, 100% 43%, 94% 78%, 68% 100%, 32% 100%, 6% 78%, 0% 43%, 17% 12%);">
                                 @if (!empty($profilePhoto))
-                                    <img src="{{ config('filesystems.disks.azure.url') . '/' . $profilePhoto }}" alt="{{ $fullname }}" class='w-full h-full object-cover'>
+                                    <img src="{{ config('filesystems.disks.azure.url') . '/' . $profilePhoto }}" alt="{{ $fullname }}" class='w-full h-full object-cover opacity-80'>
                                 @else
-                                    <p class='text-paragraph font-primary font-bold text-primary-70 h-fit'>{{ Str::charAt(Str::upper($fullname), 0) }}</p>
+                                    <img src="{{ asset('assets/defaults/user.jpg') }}" alt="userDefault" class='w-full h-full object-cover opacity-75'>
                                 @endif
-                                {{-- <img src="w-full h-full object-cover" alt=""> --}}
-                            {{-- </div>
+                            </div>
                         </div>
-                        <div class='flex flex-col items-center mb-2'>
-                            <p>{{ $fullname }}</p>
-                            <p>{{ '@' . $username }}</p>
+                        <div class='flex flex-col gap-3'>
+                            <div class='flex flex-col items-start'>
+                                <p class='text-body-size font-bold {{ $textMoodStyle[$mood] }}'>{{ Str::limit($fullname, 18) }}</p>
+                                <p class='text-small italic'>{{ '@' . $username }}</p>
+                            </div>
+                            <div class='flex flex-col'>
+                                <div class='flex flex-row items-center justify-center gap-2 text-micro'>
+                                    <h1 class="">{{ $user->followings()->count() . ' Follower' . (($user->followings()->count() > 1) ? 's' : '') }}</h1>
+                                    <div class='{{ $bgBasedMood[$mood] }} rounded-full w-2 h-2'></div>
+                                    <h1 class="">{{ $user->followers()->count() . ' Following' }}</h1>
+                                </div>
+                                <x-button :mood='$mood' content='Following' class='w-max px-2' style="zoom:0.85;"></x-button>
+                            </div>
                         </div>
-                        <div class='flex flex-row items-center justify-center gap-2 mb-3'>
-                            <h1 class="">{{ $user->followings()->count() . ' Follower' . (($user->followings()->count() > 1) ? 's' : '') }}</h1>
-                            <div class='{{ $bgBasedMood[$mood] }} rounded-full w-2 h-2'></div>
-                            <h1 class="">{{ $user->followers()->count() . ' Following' }}</h1>
-                        </div>
-                        <div class='w-full'>
-                        <x-button :mood='$mood'></x-button>
-                        </div>
-                    </div> --}} 
-
+                    </div>
+                    
                     <div class='relative w-full h-84 rounded-lg overflow-hidden hidden group md:inline'>
                         @if (!empty($profilePhoto))
                             <img src="{{ config('filesystems.disks.azure.url') . '/' . $profilePhoto }}" alt="{{ $username }}" class='w-full h-full object-cover opacity-80'>
                         @else
                             <img src="{{ asset('assets/defaults/user.jpg') }}" alt="userDefault" class='w-full h-full object-cover opacity-75'>
                         @endif
-                        <div class='absolute flex flex-col gap-2 w-full h-2/5 bg-linear-to-t from-shadedOfGray-85 to-white/30 bottom-0 left-0 group-hover:border-t-4 group-hover:border-white duration-150' style="clip-path: polygon(0% 38%, 55% 38%, 65% 0%, 100% 0%, 100% 100%, 0% 100%);">
+                        <div class='absolute flex flex-col gap-2 w-full h-2/5 bg-linear-to-t from-shadedOfGray-85 to-white/30 bottom-0 left-0 group-hover:border-t-4 group-hover:border-white duration-150' style="clip-path: polygon(0% 38%, 45% 38%, 55% 0%, 100% 0%, 100% 100%, 0% 100%);">
                             <div class='w-full h-max flex justify-end' style="zoom:0.725;">
                                 <x-button content='Following' :mood='$mood' class='w-max mr-3 mt-3.5'></x-button>
                             </div>
                             <div class='w-full flex flex-col gap-1 items-start font-secondaryAndButton text-white px-3 pt-2'>
                                 <div class='flex flex-row items-center gap-2 w-full'>
-                                    <p class='font-bold {{ $textMoodStyle[$mood] }} text-body-size'>{{ Str::limit($username, 17) }}</p>
+                                    <p class='font-bold {{ $textMoodStyle[$mood] }} text-body-size'>{{ Str::limit($fullname, 17) }}</p>
                                     <p class='font-bold {{ $pipeMoodStyle[$mood] }}'>|</p>
                                     <p class='text-small italic'>{{ '@' . $username }}</p>
                                 </div>
@@ -151,6 +152,6 @@
             @endforeach
         </div>
     @else
-        <p class='w-full text-white font-secondaryAndButton text-body-size'>There is no other user(s) can be displayed.</p>
+        <p class='w-full text-white font-secondaryAndButton text-small lg:text-body-size'>There is no other user(s) can be displayed.</p>
     @endif
 @endsection
