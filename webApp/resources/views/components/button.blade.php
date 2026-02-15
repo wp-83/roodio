2507ebc5237ee3268f4d9974456dde08be7e9ab5
William P. <williampratama837@gmail.com>
2026-02-15 22:01:12 +0700

    update: disabled style

--- webApp/resources/views/components/button.blade.php
+++ webApp/resources/views/components/button.blade.php
@@ -12,15 +12,15 @@
 @php
     $navLink ??= route('welcome');
 
-    $baseStyle = 'relative overflow-hidden w-full px-4 py-1 my-1 font-secondaryAndButton rounded-3xl cursor-pointer text-mediumBtn text-center font-bold before:absolute before:inset-0 before:-translate-x-full before:transition-transform before:duration-400 hover:before:translate-x-0 md:text-largeBtn md:px-5 md:py-1.5';
+    $baseStyle = 'relative overflow-hidden w-full px-4 py-1 my-1 font-secondaryAndButton rounded-3xl cursor-pointer text-mediumBtn text-center font-bold before:absolute before:inset-0 before:-translate-x-full before:transition-transform before:duration-400 hover:before:translate-x-0 md:text-largeBtn md:px-5 md:py-1.5 disabled:hover:before:-translate-x-full disabled:opacity-70 disabled:cursor-not-allowed disabled:bg-shadedOfGray-30';
 
     $moodStyle = [
-      'happy' => 'bg-secondary-happy-20 text-secondary-happy-100 before:bg-secondary-happy-85 hover:text-secondary-happy-10',
-      'sad' => 'bg-secondary-sad-20 text-secondary-sad-100 before:bg-secondary-sad-85 hover:text-secondary-sad-10',
-      'relaxed' => 'bg-secondary-relaxed-20 text-secondary-relaxed-100 before:bg-secondary-relaxed-85 hover:text-secondary-relaxed-10',
-      'angry' => 'bg-secondary-angry-20 text-secondary-angry-100 before:bg-secondary-angry-85 hover:text-secondary-angry-10',
+      'happy' => 'bg-secondary-happy-20 text-secondary-happy-100 before:bg-secondary-happy-85 hover:text-secondary-happy-10 disabled:hover:text-secondary-happy-100',
+      'sad' => 'bg-secondary-sad-20 text-secondary-sad-100 before:bg-secondary-sad-85 hover:text-secondary-sad-10 disabled:hover:text-secondary-sad-100',
+      'relaxed' => 'bg-secondary-relaxed-20 text-secondary-relaxed-100 before:bg-secondary-relaxed-85 hover:text-secondary-relaxed-10 disabled:hover:text-secondary-relaxed-100',
+      'angry' => 'bg-secondary-angry-20 text-secondary-angry-100 before:bg-secondary-angry-85 hover:text-secondary-angry-10 disabled:hover:text-secondary-angry-100',
       'grayscale' => 'bg-shadedOfGray-10 text-shadedOfGray-60',
-      'default' => 'bg-primary-10 text-primary-85 before:bg-primary-50 hover:text-white'
+      'default' => 'bg-primary-10 text-primary-70 before:bg-primary-50 hover:text-white disabled:hover:text-primary-70'
     ];
 
     $moodNavStyle = [
