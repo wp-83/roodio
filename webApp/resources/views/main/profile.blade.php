d23ec38b3e5ad2f10f2ea3d7188a58db802b6ed6
William P. <williampratama837@gmail.com>
2026-02-16 00:23:43 +0700

    fix: layouting and logic profile page

--- webApp/resources/views/main/profile.blade.php
+++ webApp/resources/views/main/profile.blade.php
@@ -9,4 +9,26 @@
 
 @section('bodyContent')
     @livewire('user.profile');
+    {{-- <script>
+        const setupSelect = (id) => {
+            const el = document.querySelector(id);
+            if (!el) return;
+
+            const update = () => {
+            if (el.value) {
+                el.classList.add('bg-accent-20/60', 'text-shadedOfGray-100');
+                el.classList.remove('text-shadedOfGray-60', 'bg-shadedOfGray-20/50', 'italic', 'bg-error-lighten/25');
+            } else if(!el.classList.contains('error')) {
+                el.classList.remove('bg-accent-20/60', 'text-shadedOfGray-100');
+                el.classList.add('italic', 'bg-shadedOfGray-20/50', 'bg-error-lighten/25', 'border-error-dark');
+            }
+            };
+
+            update();
+            el.addEventListener('change', update);
+        };
+
+        setupSelect('#gender');
+        setupSelect('#country');
+    </script> --}}
 @endsection
