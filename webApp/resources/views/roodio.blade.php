6eedfa963b732ad2e0bc556ad65ee0a1c6d5d337
William P. <williampratama837@gmail.com>
2026-02-16 11:11:44 +0700

    update: landing page

--- webApp/resources/views/roodio.blade.php
+++ webApp/resources/views/roodio.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 
-@section('title', 'ROODIO | Music Player Based on Your Mood')
+@section('title', 'ROODIO - Music Player Based on Your Mood')
 
 
 @push('style')
@@ -21,7 +21,14 @@
 
 @section('bodyContent')
     <header>
-        <x-button behaviour="navigation" navLink="login" content="Login"></x-button>
+        <div class='flex flex-row items-center justify-between p-2 bg-primary-20'>
+            <div class='w-64'>
+                <img src="{{ asset('assets/logo/logo-horizontal.png') }}" alt="logo" class='w-full'>
+            </div>
+            <div class='w-max'>
+                <x-button behaviour="navigation" navLink="login" content="Let's Start!"></x-button>
+            </div>
+        </div>
     </header>
     <section>
 
